64a9c7cd0b5c550e0873a80d99138b1f71b02b1d
Robin Olsen <robinolsen98@outlook.com>
2026-02-05 13:26:52 +0100

    use tui config for consistency

--- cmd/tui/main.go
+++ cmd/tui/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"context"
 
-	"github.com/LazyBachelor/LazyPM/internal/service"
 	"github.com/LazyBachelor/LazyPM/pkg/tui"
 )
 
 func main() {
-	config := service.Config{
+	config := tui.TUIConfig{
 		StatisticsStoragePath: "./.pm/stats.json",
 		BeadsDBPath:           "./.pm/db.db",
 		IssuePrefix:           "pm",
