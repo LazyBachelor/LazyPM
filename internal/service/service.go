2215b5ad34dc8273ddb0db2cc2146871f31741b9
Robin Olsen <robinolsen98@outlook.com>
2026-02-05 15:53:15 +0100

    add promt to user to confirm initializaton in direcory

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,10 +2,13 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/LazyBachelor/LazyPM/internal/models"
 	"github.com/LazyBachelor/LazyPM/internal/storage"
+	"github.com/charmbracelet/huh"
 
 	"github.com/google/uuid"
 	"github.com/steveyegge/beads"
@@ -27,6 +30,11 @@ type Services struct {
 
 func NewServices(ctx context.Context, config Config) (*Services, func(), error) {
 	var cleanupFuncs []func()
+
+	if !initialized(config.BeadsDBPath) {
+		fmt.Println("PM is not initialized")
+		os.Exit(0)
+	}
 
 	store, err := beads.NewSQLiteStorage(ctx, config.BeadsDBPath)
 	if err != nil {
@@ -62,4 +70,25 @@ func runCleanup(funcs []func()) {
 	for _, fn := range funcs {
 		fn()
 	}
+}
+
+func initialized(beadsPath string) bool {
+	_, err := os.Stat(beadsPath)
+
+	if os.IsNotExist(err) {
+		var initialize bool
+
+		huh.NewForm(
+			huh.NewGroup(
+				huh.NewConfirm().Title("PM is not initialized in this directory!").
+					Description("Do you want to initialize it here?").
+					Value(&initialize),
+			),
+		).WithTheme(huh.ThemeBase16()).WithAccessible(true).Run()
+
+		if !initialize {
+			return false
+		}
+	}
+	return true
 }
