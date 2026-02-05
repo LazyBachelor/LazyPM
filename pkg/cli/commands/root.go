ab2fab6194a080cf69039cf36cdafb1dcf2a1b2e
Robin Olsen <robinolsen98@outlook.com>
2026-02-05 13:25:34 +0100

    use new rootCmd name var

--- pkg/cli/commands/root.go
+++ pkg/cli/commands/root.go
@@ -9,20 +9,21 @@ import (
 var svc *service.Services
 
 var rootCmd = &cobra.Command{
-	Use:   "pm",
 	Short: "Project Management CLI",
 	Long:  `Project Management CLI for managing issues and tasks.`,
 }
 
 func Execute(services *service.Services) error {
 	svc = services
+	rootCmd.Use = svc.Config.RootCmd
 	return rootCmd.Execute()
 }
 
 func init() {
 	rootCmd.AddCommand(createCmd)
+
 	rootCmd.CompletionOptions.DisableDefaultCmd = false
-	rootCmd.AddGroup(&cobra.Group{ID: "other", Title: "Helping Commands"})
-	rootCmd.SetCompletionCommandGroupID("other")
-	rootCmd.SetHelpCommandGroupID("other")
+	rootCmd.AddGroup(&cobra.Group{ID: "help", Title: "Helping Commands"})
+	rootCmd.SetCompletionCommandGroupID("help")
+	rootCmd.SetHelpCommandGroupID("help")
 }
