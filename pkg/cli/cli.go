d962ab93140acd8c2607915cca1ceb15209918f0
Robin Olsen <robinolsen98@outlook.com>
2026-02-06 23:30:59 +0100

    add run with args for running as sub command

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -1,23 +1,42 @@
 package cli
 
 import (
+	"context"
+
 	"github.com/LazyBachelor/LazyPM/internal/service"
 	"github.com/LazyBachelor/LazyPM/pkg/cli/commands"
-	"context"
 )
 
 type CLIConfig = service.Config
 
 func Run(ctx context.Context, config CLIConfig) error {
 	svc, cleanup, err := service.NewServices(ctx, config)
-
 	if err != nil {
 		return err
 	}
 
 	defer cleanup()
 
-	if err := commands.Execute(svc); err != nil {
+	commands.SetServices(svc)
+
+	if err := commands.Execute(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func RunWithArgs(ctx context.Context, config CLIConfig, args []string) error {
+	svc, cleanup, err := service.NewServices(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	defer cleanup()
+
+	commands.SetServices(svc)
+
+	if err := commands.ExecuteArgs(args); err != nil {
 		return err
 	}
 
