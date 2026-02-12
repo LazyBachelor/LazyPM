d670ec0dc22d1c67279e9e22a9d0718771f06b60
Robin Olsen <robinolsen98@outlook.com>
2026-02-12 22:07:43 +0100

    check if nil

--- cmd/survey/tasks/runner.go
+++ cmd/survey/tasks/runner.go
@@ -2,21 +2,32 @@ package tasks
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/LazyBachelor/LazyPM/internal/service"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func (t *Task) IntroduceTask() error {
+	if t.aboutScreen == nil {
+		return fmt.Errorf("aboutScreen is not set")
+	}
 	_, err := tea.NewProgram(t.aboutScreen, tea.WithAltScreen()).Run()
 	return err
 }
 
 func (t *Task) StartInterface(ctx context.Context, cfg service.Config) error {
+	if t.interfaceType == nil {
+		return fmt.Errorf("interfaceType is not set")
+	}
+
 	return t.interfaceType.Run(ctx, cfg)
 }
 
 func (t *Task) StartQuestionnaire() error {
+	if t.questionnaire == nil {
+		return fmt.Errorf("questionnaire is not set")
+	}
 	_, err := tea.NewProgram(t.questionnaire, tea.WithAltScreen()).Run()
 	return err
 }
