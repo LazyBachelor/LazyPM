412b2fe0c2b7da008061124630e9737295569c25
Robin Olsen <robinolsen98@outlook.com>
2026-02-12 22:06:48 +0100

    add config to tasks use fmt.Errorf

--- cmd/survey/tasks/task.go
+++ cmd/survey/tasks/task.go
@@ -3,43 +3,55 @@ package tasks
 
 import (
 	"context"
-	"errors"
+	"fmt"
 
 	"github.com/LazyBachelor/LazyPM/internal/service"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-func NewTask(interfaceType Interface, aboutScreen tea.Model, questionnaire tea.Model) *Task {
+type Task struct {
+	Config        TaskConfig
+	interfaceType Interface
+	aboutScreen   tea.Model
+	questionnaire tea.Model
+
+	validateFunc ValidateFunc
+	dbStateFunc  DbStateFunc
+}
+
+func NewTask(aboutScreen tea.Model, questionnaire tea.Model) *Task {
 	return &Task{
-		interfaceType: interfaceType,
 		aboutScreen:   aboutScreen,
 		questionnaire: questionnaire,
 	}
 }
 
-func (t *Task) SetValidateFunc(fn ValidateFunc) {
-	t.validateFunc = fn
-}
-
-func (t *Task) SetDbStateFunc(fn DbStateFunc) {
-	t.dbStateFunc = fn
+func (t *Task) SetConfigFunc(fn ConfigFunc) {
+	t.Config = fn()
 }
 
 func (t *Task) SetInterface(interfaceType Interface) {
 	t.interfaceType = interfaceType
 }
 
-func (t *Task) Validate(ctx context.Context, svc *service.Services) (bool, error) {
-	if t.validateFunc == nil {
-		return false, errors.New("validateFunc is not set")
-	}
-	return t.validateFunc(ctx, svc)
+func (t *Task) SetDbStateFunc(fn DbStateFunc) {
+	t.dbStateFunc = fn
 }
 
-func (t *Task) MigrateToTask(ctx context.Context, svc *service.Services, task *Task) error {
-	t = task
+func (t *Task) SetValidateFunc(fn ValidateFunc) {
+	t.validateFunc = fn
+}
+
+func (t *Task) Initialize(ctx context.Context, svc *service.Services) error {
 	if t.dbStateFunc == nil {
-		return errors.New("dbStateFunc is not set")
+		return fmt.Errorf("dbStateFunc is not set")
 	}
 	return t.dbStateFunc(ctx, svc)
+}
+
+func (t *Task) Validate(ctx context.Context, svc *service.Services) (bool, error) {
+	if t.validateFunc == nil {
+		return false, fmt.Errorf("validateFunc is not set")
+	}
+	return t.validateFunc(ctx, svc)
 }
