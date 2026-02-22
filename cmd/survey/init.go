78769396e7c000919d1775c337115fa511f6bd53
Robin Olsen <robinolsen98@outlook.com>
2026-02-22 19:47:17 +0100

    forgot to commit this

--- cmd/survey/init.go
+++ cmd/survey/init.go
@@ -3,38 +3,15 @@ package main
 import (
 	"context"
 
+	"github.com/LazyBachelor/LazyPM/cmd/survey/tasks"
 	"github.com/LazyBachelor/LazyPM/internal/service"
-	"github.com/LazyBachelor/LazyPM/pkg/cli/repl"
+	"github.com/LazyBachelor/LazyPM/pkg/repl"
 	"github.com/LazyBachelor/LazyPM/pkg/task"
 	"github.com/LazyBachelor/LazyPM/pkg/tui"
 	"github.com/LazyBachelor/LazyPM/pkg/web"
 
-	"github.com/LazyBachelor/LazyPM/cmd/survey/tasks"
 	_ "github.com/LazyBachelor/LazyPM/cmd/survey/tasks"
 )
-
-func init() {
-	task.Register("create_issue", func(app *service.App) task.Tasker {
-		return tasks.NewCreateIssueTask(app)
-	})
-	task.Register("coding_task", func(app *service.App) task.Tasker {
-		return tasks.NewCodingTask(app)
-	})
-}
-
-func initTasks(app *service.App) []task.Tasker {
-	var taskers []task.Tasker
-
-	for _, name := range task.List() {
-		t, err := task.Get(name, app)
-		if err != nil {
-			continue
-		}
-		taskers = append(taskers, t)
-	}
-
-	return taskers
-}
 
 func initializeServices(ctx context.Context) (*service.App, func(), error) {
 	return service.NewServices(ctx, tasks.BaseConfig())
@@ -46,4 +23,18 @@ func initInterfaces() map[string]task.Interface {
 		"tui":  tui.NewTui(),
 		"web":  web.NewWeb(),
 	}
+}
+
+func initTasks(app *service.App) []task.Tasker {
+	var taskList []task.Tasker
+
+	for _, name := range task.List() {
+		t, err := task.Get(name, app)
+		if err != nil {
+			continue
+		}
+		taskList = append(taskList, t)
+	}
+
+	return taskList
 }
